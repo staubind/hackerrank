2b10c4ee115dc3e5eb14e159e3eccafd2cf83c6d
Dan St. Aubin <staubindj@gmail.com>
2021-11-26 22:02:31 -0600

    doesnt respect spacing for now

--- title_case.py
+++ title_case.py
@@ -1,14 +1,16 @@
 import re
-
 def solve(s):
-
-    # Capitalize in Python - HackerRank Solution START
-    for i in s.split():
-        s = s.replace(i,i.capitalize())
-    return s
+    names = s.split()
+    title_names = []
+    for name in names:
+        if name[0].isdigit():
+            title_names.append(name)
+        else:
+            title_names.append(name.title())
+    return ' '.join(title_names)
 
 print(solve('1 w 2 r 3g'))
 print(solve('hello   world  lol'))
 print(solve('132 456 Wq  M E'))
-print(solve('hihi ihihihi fabulos, 12abc'))
+
 # need to respect spaces
