847f0bfc9f6330a1260539bb4a458152581d81ea
Dan St. Aubin <staubindj@gmail.com>
2021-12-09 23:56:40 -0600

    added new hackerrank problem

---
+++ sets/math/mod_power.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def mod_power():
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    print(a**b)
+    print(a**b % m)
+    
+mod_power()
