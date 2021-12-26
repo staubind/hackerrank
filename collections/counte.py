1f648996a8d597f3c0f42ffc26aabc9564ab4519
Dan St. Aubin <staubindj@gmail.com>
2021-12-25 21:01:02 -0600

    started a hackerrank problem,

---
+++ collections/counte.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from collections import Counter
+
+def count_stuff():
+    pass    
+    
+num_shoes = int(input())
+shoe_sizes = list(map(int, input().split()))
+num_customers = int(input())
+desired_sizes = []
+for i in range(num_customers):
+    desired_sizes.append(int(input())
+    
