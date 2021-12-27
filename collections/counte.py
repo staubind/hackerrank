051ff9e7996e727483a62de4cb0743d844491386
Dan St. Aubin <staubindj@gmail.com>
2021-12-26 20:20:53 -0600

    finished a counter functin

--- collections/counte.py
+++ collections/counte.py
@@ -2,13 +2,24 @@
 
 from collections import Counter
 
-def count_stuff():
-    pass    
+def calculate_price(supply, demand, s):
+    total_earned = 0
+    supply = Counter(supply)
+    for i in range(len(demand)):
+        size = demand[i]
+        if supply[size]:
+            total_earned += prices[i]
+            supply[size] -= 1
+    return total_earned
     
 num_shoes = int(input())
 shoe_sizes = list(map(int, input().split()))
 num_customers = int(input())
 desired_sizes = []
+prices = []
 for i in range(num_customers):
-    desired_sizes.append(int(input())
-    
+    inpt = input().split()
+    desired_sizes.append(int(inpt[0]))
+    prices.append(int(inpt[1]))
+print(calculate_price(shoe_sizes, desired_sizes, prices))
+
